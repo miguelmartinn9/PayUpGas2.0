bb3f2197e3818d3517198e8911f539e2ad33cbdd
nerviosus <nerviosus@protonmail.com>
2019-10-12 17:09:35 +0200

    Last commit (Hopefully), the back-end works just fine and the front-end is as shitty as usual

--- screens/HelloScreen.js
+++ screens/HelloScreen.js
@@ -6,13 +6,16 @@ import {
     TextInput,
     TouchableOpacity,
 } from 'react-native';
+import { FlatList, ActivityIndicator} from 'react-native';
 
+var sumaGas = 0;
 let precioGasolina = 1.235;
 let pricePerFucker;
+var gass;
 
 function pricePerPass(pass,km,liters) {
     let litrosAGastar = (km*liters)/100;
-    let precioTotal = litrosAGastar*precioGasolina;
+    let precioTotal = litrosAGastar*Number((sumaGas).toFixed(4));
     pricePerFucker = precioTotal/4;
 }
 
@@ -20,12 +23,45 @@ class HelloScreen extends React.Component{
 
     constructor(props) {
         super(props);
-        this.state={passengers: '', kilometers:'', litersPKM:''};
+        this.state={passengers: '', kilometers:'', litersPKM:'', isLoading: true};
+    }
+    componentDidMount(){
+        return fetch('https://www.mapabase.es/arcgis/rest/services/Otros/Gasolineras/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json')
+            .then((response) => response.json())
+            .then((responseJson) => {
+
+                const primeros20 = responseJson.features.
+                slice(0,20).
+                map(e=>e.attributes.precio_gasóleo_a);
+
+                console.log(primeros20);
+                gass = primeros20;
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson.features,
+                }, function(){
+
+                });
+
+            })
+            .catch((error) =>{
+                console.error(error);
+            });
     }
 
     render (){
+        if(this.state.isLoading){
+            return(
+                <View style={{flex: 1, padding: 20}}>
+                    <ActivityIndicator/>
+                </View>
+            )
+        }
         return(
             <View>
+                <View style={styleHidden.hide}>
+                    <Text style={styleHidden.hide}>{sumarGas()}</Text>
+                </View>
                 <View style={stylesss.inputWrapper}>
                     <TextInput
                         placeholder={'Number of passengers'}
@@ -52,14 +88,26 @@ class HelloScreen extends React.Component{
                 </View>
 
                 <View style={stylesss.resultado}>
-                    <Text style={{fontSize:20, textAlign: 'center'}}>Precio por pasajero: {pricePerFucker}</Text>
+                    <Text style={{fontSize:20, textAlign: 'center'}}>Precio por pasajero: {Number((pricePerFucker).toFixed(4))}</Text>
                 </View>
+
             </View>
         );
     }
 }
 
-
+function sumarGas () {
+    for (var i = 0; i<20;i++) {
+        sumaGas += gass[i];
+    }
+    sumaGas = sumaGas/20;
+    return Number((sumaGas).toFixed(4))
+}
+const styleHidden = StyleSheet.create({
+    hide: {
+        color: "white",
+    },
+});
 
 const stylesss = StyleSheet.create({
     resultado: {
